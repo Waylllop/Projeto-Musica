21414ab0b460a7ffea4b66bfa905d1522b5ecca1
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-12-27 22:18:31 -0300

    adiciona foto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <div>
       <h1>Musica</h1>
+      <img
+        style={{ width: 600, height: 600 }}
+        src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Cover-VOID.png"
+        alt="VOID"
+      ></img>
       <audio src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Kimochi.mp3" controls></audio>
     </div>
   );
