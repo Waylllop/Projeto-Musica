5baa00061ad17ee0ee5e9cd9556ba98d42c0b7ec
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-12-28 01:14:08 -0300

    diversas configuracoes iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <h1>Musica</h1>
-      <img
-        style={{ width: 600, height: 600 }}
-        src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Cover-VOID.png"
-        alt="VOID"
-      ></img>
-      <audio src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Kimochi.mp3" controls></audio>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Musica</h1>
+      <img
+        style={{ width: 600, height: 600 }}
+        src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Cover-VOID.png"
+        alt="VOID"
+      ></img>
+      <audio src="https://waylllop.github.io/MusicasMp3/into-the-void-EP/Kimochi.mp3" controls></audio>
+    </div>
+  );
+};
+
+export default Home;
