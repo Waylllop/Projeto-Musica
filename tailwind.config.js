3e7851ee2090c48fab4b1f0d3b38e79616f47071
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-12-29 18:40:11 -0300

    adiciona configuracoes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cabin: ["Cabin", "sans-serif"],
+        coustard: ["Coustard", "serif"],
+      },
+      colors: {
+        primary: "#FBB13C",
+        secondary: "#895B1E",
+        light: "#D1DEDE",
+        dark: "#1B2021",
+        bgLight: "#D9D9D9",
+      },
+    },
   },
   plugins: [],
-}
+};
