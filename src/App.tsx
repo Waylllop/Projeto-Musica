f78b55214170e1c0f2a804194c4a783189f54737
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-12-28 22:14:48 -0300

    adiciona apollo e graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 
+const client = new ApolloClient({
+  uri: "https://api-sa-east-1.hygraph.com/v2/clqpoqfjy1ux101un6a2w61pa/master",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ApolloProvider>
   );
 }
 
