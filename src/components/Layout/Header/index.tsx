2d6a110d5424afd3b3c786a5ffd64b89c60dea43
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-12-29 22:01:30 -0300

    ajusta header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,19 +1,32 @@
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 
 const Header = () => {
+  const location = useLocation();
+  const [isSongsOpen, setIsSongsOpen] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === "/songs") {
+      setIsSongsOpen(true);
+    } else {
+      setIsSongsOpen(false);
+    }
+  }, [location.pathname]);
+
   return (
-    <div className="bg-dark mb-4 fixed w-full flex justify-between px-16">
-      <NavLink to="/home">
-        <img
-          className="w-[200px]"
-          src="https://waylllop.github.io/MusicasMp3/identidade-visual/logo-oranje.png"
-          alt="Logo Yllop"
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        />
+    <div className="bg-dark mb-4 fixed w-full flex justify-between px-16 py-4">
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `font-coustard text-primary text-4xl ${isActive ? "duration-200" : "hover:text-[#fcc46a] duration-200]"}`
+        }
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        LL
       </NavLink>
-      <nav className="flex items-center pl-2">
-        <ul className="flex gap-6 text-white text-lg">
+      <nav className="flex items-center">
+        <ul className="flex gap-6 text-white text-xl">
           <li>
             <NavLink
               to="/home"
@@ -25,52 +38,46 @@ const Header = () => {
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/songs"
-              className={({ isActive }) =>
-                isActive ? "text-primary duration-200" : "hover:text-[#fcc46a] duration-200"
-              }
-            >
-              Songs
-            </NavLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="work"
-              spy={true}
-              smooth={true}
-              offset={-100}
-              duration={500}
-              className="cursor-pointer hover:text-[#fcc46a]"
-            >
-              Work
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={-100}
-              duration={500}
-              className="cursor-pointer hover:text-[#fcc46a]"
-            >
-              About
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={-100}
-              duration={500}
-              className="cursor-pointer hover:text-[#fcc46a]"
-            >
-              Contact
-            </ScrollLink>
-          </li>
+          {!isSongsOpen ? (
+            <>
+              <li>
+                <ScrollLink
+                  to="work"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={1000}
+                  className="cursor-pointer hover:text-[#fcc46a]"
+                >
+                  Work
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={1000}
+                  className="cursor-pointer hover:text-[#fcc46a]"
+                >
+                  About
+                </ScrollLink>
+              </li>
+              <li>
+                <ScrollLink
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={1000}
+                  className="cursor-pointer hover:text-[#fcc46a]"
+                >
+                  Contact
+                </ScrollLink>
+              </li>
+            </>
+          ) : null}
         </ul>
       </nav>
     </div>
--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 const Work = () => {
   return (
     <section id="work" className="scroll-smooth mx-16 mt-20 border-t-4 border-dark">
       <h1 className="my-10 text-5xl font-bold">Work</h1>
-      <div className="bg-secondary h-[600px]"></div>
+      <div className="bg-secondary h-[600px]">
+        <NavLink
+          to="/songs"
+          className={({ isActive }) =>
+            `font-coustard text-primary text-4xl ${isActive ? "duration-200" : "hover:text-[#fcc46a] duration-200]"}`
+          }
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          Songs
+        </NavLink>
+      </div>
     </section>
   );
 };
