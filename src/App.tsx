71331bdd1c9eba327c7b239bff23a697fcbdd8d2
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2024-01-08 21:30:27 -0300

    troca chamada da api

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,39 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from "@apollo/client";
 import { SongProvider } from "./context/SongContext";
 import { SongPlayingProvider } from "./context/SongPlaying";
-// import { setContext } from "@apollo/client/link/context";
+import { setContext } from "@apollo/client/link/context";
 
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Work from "./pages/Work";
 
-// const httpLink = createHttpLink({
-//   uri: `${import.meta.env.VITE_HOST}`,
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = `${import.meta.env.VITE_PERMANENT_AUTH_TOKEN}`;
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
-
 const httpLink = createHttpLink({
-  uri: "https://api-sa-east-1.hygraph.com/v2/clqpoqfjy1ux101un6a2w61pa/master",
+  uri: `${import.meta.env.VITE_HOST}`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = `${import.meta.env.VITE_PERMANENT_AUTH_TOKEN}`;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+// const httpLink = createHttpLink({
+//   uri: "https://api-sa-east-1.hygraph.com/v2/clqpoqfjy1ux101un6a2w61pa/master",
+// });
+
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
